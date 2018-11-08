fca779e449c9f41f4e9d57e9cb428e169741f216
ROOT\amalraj.umapathy <amalraj.umapathy@syncfusion.com>
2018-11-08 17:40:11 +0530

    Sample included for Drag drop between TreeGrid and TreeViewadv.

---
+++ Drag drop between TreeGrid and TreeView/DragDropBehavior.cs
@@ -0,0 +1,238 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using System.Windows.Media;
+using Syncfusion.UI.Xaml.TreeGrid;
+using Syncfusion.UI.Xaml.TreeGrid.Helpers;
+using Syncfusion.Data.Extensions;
+
+namespace Drag_drop_between_TreeGrid_and_TreeView
+{
+    /// <summary>
+    /// DragDropBehavior model class
+    /// </summary>
+   public class DragDropBehavior:Behavior<MainWindow>
+   {
+        /// <summary>
+        /// Initialize the OnAttached method
+        /// </summary>
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.Loaded += AssociatedObject_Loaded;
+        }
+
+        /// <summary>
+        /// Wire the loaded event of this class
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AssociatedObject_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.AssociatedObject.sfTreeGrid.RowDragDropController.DragStart += RowDragDropController_DragStart;
+            this.AssociatedObject.sfTreeGrid.RowDragDropController.Drop += RowDragDropController_Drop;
+            this.AssociatedObject.treeview.Drop += Treeview_Drop;
+        }
+
+        /// <summary>
+        /// Describes the Drop event of the TreeView. It handled the drop items from SfTreeGrid to TreeViewAdv
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Treeview_Drop(object sender, DragEventArgs e)
+        {
+            ObservableCollection<TreeNode> treeNodes = new ObservableCollection<TreeNode>();
+
+            if (e.Data.GetDataPresent("Nodes"))
+                treeNodes = e.Data.GetData("Nodes") as ObservableCollection<TreeNode>;
+
+            EmployeeInfo item = new EmployeeInfo();
+
+            if (treeNodes.Count == 0 || treeNodes == null)
+                return;
+
+            foreach (var node in treeNodes)
+            {
+                (AssociatedObject.sfTreeGrid.ItemsSource as ObservableCollection<EmployeeInfo>).Remove(node.Item as EmployeeInfo);
+            }
+        }
+
+        /// <summary>
+        /// Describes the Drop event of the RowDragDropController. It handled the drop items from TreeVideAdv to SfTreeGrid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RowDragDropController_Drop(object sender, TreeGridRowDropEventArgs e)
+        {
+            if (e.IsFromOutSideSource)
+            {
+                ObservableCollection<object> item = e.Data.GetData(typeof(ObservableCollection<object>)) as ObservableCollection<object>;
+                var record = item[0] as EmployeeInfo;
+                var dropPosition = e.DropPosition.ToString();
+                var newItem = new EmployeeInfo();
+
+                var rowIndex = AssociatedObject.sfTreeGrid.ResolveToRowIndex(e.TargetNode.Item);
+                int nodeIndex = (int)rowIndex;
+                if (dropPosition != "None" && rowIndex != -1)
+                {
+                    if (AssociatedObject.sfTreeGrid.View is TreeGridSelfRelationalView)
+                    {
+                        var treeNode = AssociatedObject.sfTreeGrid.GetNodeAtRowIndex(rowIndex);
+
+                        if (treeNode == null)
+                            return;
+                        var data = treeNode.Item;
+                        AssociatedObject.sfTreeGrid.SelectionController.SuspendUpdates();
+                        var itemIndex = -1;
+
+                        TreeNode parentNode = null;
+
+                        if (dropPosition == "DropBelow" || dropPosition == "DropAbove")
+                        {
+                            parentNode = treeNode.ParentNode;
+
+                            if (parentNode == null)
+                                newItem = new EmployeeInfo() { FirstName = record.FirstName, LastName = record.LastName, ID = record.ID, Salary = record.Salary, Title = record.Title, ReportsTo = -1 };
+                            else
+                            {
+                                var parentkey = parentNode.Item as EmployeeInfo;
+                                newItem = new EmployeeInfo() { FirstName = record.FirstName, LastName = record.LastName, ID = record.ID, Salary = record.Salary, Title = record.Title, ReportsTo = parentkey.ID };
+                            }
+                        }
+
+                        else if (dropPosition == "DropAsChild")
+                        {
+
+                            if (!treeNode.IsExpanded)
+                                AssociatedObject.sfTreeGrid.ExpandNode(treeNode);
+                            parentNode = treeNode;
+                            var parentkey = parentNode.Item as EmployeeInfo;
+                            newItem = new EmployeeInfo() { FirstName = record.FirstName, LastName = record.LastName, ID = record.ID, Salary = record.Salary, Title = record.Title, ReportsTo = parentkey.ID };
+
+                        }
+                        IList sourceCollection = null;
+
+                        if (dropPosition == "DropBelow" || dropPosition == "DropAbove")
+                        {
+
+                            if (treeNode.ParentNode != null)
+                            {
+
+                                var collection = AssociatedObject.sfTreeGrid.View.GetPropertyAccessProvider().GetValue(treeNode.ParentNode.Item, AssociatedObject.sfTreeGrid.ChildPropertyName) as IEnumerable;
+
+                                sourceCollection = GetSourceListCollection(collection);
+                            }
+
+                            else
+                            {
+                                sourceCollection = GetSourceListCollection(AssociatedObject.sfTreeGrid.View.SourceCollection);
+                            }
+                            itemIndex = sourceCollection.IndexOf(data);
+
+                            if (dropPosition == "DropBelow")
+                            {
+                                itemIndex += 1;
+                            }
+                        }
+
+                        else if (dropPosition == "DropAsChild")
+                        {
+                            var collection = AssociatedObject.sfTreeGrid.View.GetPropertyAccessProvider().GetValue(data, AssociatedObject.sfTreeGrid.ChildPropertyName) as IEnumerable;
+
+                            sourceCollection = GetSourceListCollection(collection);
+
+                            if (sourceCollection == null)
+                            {
+                                var list = data.GetType().GetProperty(AssociatedObject.sfTreeGrid.ChildPropertyName).PropertyType.CreateNew() as IList;
+
+                                if (list != null)
+                                {
+                                    AssociatedObject.sfTreeGrid.View.GetPropertyAccessProvider().SetValue(treeNode.Item, AssociatedObject.sfTreeGrid.ChildPropertyName, list);
+                                    sourceCollection = list;
+                                }
+                            }
+                            itemIndex = sourceCollection.Count;
+                        }
+                        sourceCollection.Insert(itemIndex, newItem);
+                        AssociatedObject.sfTreeGrid.SelectionController.ResumeUpdates();
+                        (AssociatedObject.sfTreeGrid.SelectionController as TreeGridRowSelectionController).RefreshSelection();
+                        e.Handled = true;
+                    }
+                }
+                (AssociatedObject.treeview.ItemsSource as ObservableCollection<EmployeeInfo>).Remove(record as EmployeeInfo);
+            }
+        }
+        ObservableCollection<object> records = new ObservableCollection<object>();
+        
+        /// <summary>
+        /// Describes the DragStart event of the RowDragDropController. It handled the drag start operaion of SfTreeGrid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RowDragDropController_DragStart(object sender, TreeGridRowDragStartEventArgs e)
+        {
+            e.Handled = true;
+            var dataObject = new DataObject();
+            dataObject.SetData("SourceTreeGrid", this.AssociatedObject.sfTreeGrid);
+            dataObject.SetData("Nodes", e.DraggingNodes);
+            
+            foreach (var node in e.DraggingNodes)
+            {
+                if (node.HasChildNodes)
+                {
+                    records.Add(node.Item as EmployeeInfo);
+                    GetChildNodes(node);
+                }
+                else
+                {
+                    records.Add(node.Item as EmployeeInfo);
+                }
+            }
+
+            dataObject.SetData(records);
+
+            if(records!=null)
+            DragDrop.DoDragDrop(this.AssociatedObject.sfTreeGrid, dataObject, DragDropEffects.Copy | DragDropEffects.Move);
+            records.Clear();
+        }
+
+        /// <summary>
+        /// Get the child nodes from parent node
+        /// </summary>
+        /// <param name="node"></param>
+        private void GetChildNodes(TreeNode node)
+        {
+            foreach (var childNode in node.ChildNodes)
+            {
+                records.Add(childNode.Item as EmployeeInfo);
+                GetChildNodes(childNode);
+            }
+        }
+
+        /// <summary>
+        /// Get the source list collection
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        private IList GetSourceListCollection(IEnumerable collection)
+        {
+            IList list = null;
+            if (collection == null)
+                collection = this.AssociatedObject.sfTreeGrid.View.SourceCollection;
+            if ((collection as IList) != null)
+            {
+                list = collection as IList;
+            }
+            return list;
+        }
+    }
+}
---
+++ Drag drop between TreeGrid and TreeView/ViewModel.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drag_drop_between_TreeGrid_and_TreeView
+{
+    public class ViewModel
+    {
+        #region Private Variables
+        private ObservableCollection<EmployeeInfo> _employees;
+        private ObservableCollection<EmployeeInfo> _employee;
+        #endregion
+
+        #region ctr
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ViewModel"/> class.
+        /// </summary>
+        public ViewModel()
+        {
+            this.Employees = GetEmployeesDetails();
+            this.Employee = GetEmployees();
+        }
+
+        #endregion
+
+        #region Public properties
+        /// <summary>
+        /// Contains the details of Employees
+        /// </summary>
+        public ObservableCollection<EmployeeInfo> Employees
+        {
+            get { return _employees; }
+            set
+            {
+                _employees = value;
+
+            }
+        }
+
+        /// <summary>
+        /// Contains the details of Employee
+        /// </summary>
+        public ObservableCollection<EmployeeInfo> Employee
+        {
+            get { return _employee; }
+            set
+            {
+                _employee = value;
+
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Get the Employee details 
+        /// </summary>
+        /// <returns></returns>
+        private ObservableCollection<EmployeeInfo> GetEmployeesDetails()
+        {
+            ObservableCollection<EmployeeInfo> employeeDetails = new ObservableCollection<EmployeeInfo>();
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Ferando", LastName = "Joseph", Title = "Management", Salary = 2000000, ReportsTo = -1, ID = 2 });
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "John", LastName = "Adams", Title = "Accounts", Salary = 2000000, ReportsTo = -1, ID = 3 });
+
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Ferando1", LastName = "Joseph1", Title = "Management1", Salary = 20000000, ReportsTo = -1, ID = 4 });
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "John1", LastName = "Adams1", Title = "Accounts1", Salary = 20000000, ReportsTo = -1, ID = 5 });
+
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Ferando2", LastName = "Joseph2", Title = "Management2", Salary = 20000001, ReportsTo = -1, ID = 6 });
+
+
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Andrew", LastName = "Fuller", ID = 9, Salary = 1200000, ReportsTo = 2, Title = "Vice President" });
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Janet", LastName = "Leverling", ID = 10, Salary = 1000000, ReportsTo = 2, Title = "GM" });
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Steven", LastName = "Buchanan", ID = 11, Salary = 900000, ReportsTo = 2, Title = "Manager" });
+
+            // Accounts
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Nancy", LastName = "Davolio", ID = 12, Salary = 850000, ReportsTo = 3, Title = "Accounts Manager" });
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Margaret", LastName = "Peacock", ID = 13, Salary = 700000, ReportsTo = 3, Title = "Accountant" });
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Michael", LastName = "Suyama", ID = 14, Salary = 700000, ReportsTo = 3, Title = "Accountant" });
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Robert", LastName = "King", ID = 15, Salary = 650000, ReportsTo = 3, Title = "Accountant" });
+
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Nancy1", LastName = "Davolio1", ID = 16, Salary = 850000, ReportsTo = 4, Title = "Accounts Manager1" });
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Margaret1", LastName = "Peacock1", ID = 17, Salary = 700000, ReportsTo = 4, Title = "Accountant1" });
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Michael1", LastName = "Suyama1", ID = 18, Salary = 700000, ReportsTo = 4, Title = "Accountant1" });
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Robert1", LastName = "King1", ID = 19, Salary = 650000, ReportsTo = 5, Title = "Accountant1" });
+
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Nancy2", LastName = "Davolio2", ID = 20, Salary = 850000, ReportsTo = 5, Title = "Accounts Manager2" });
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Margaret2", LastName = "Peacock2", ID = 21, Salary = 700000, ReportsTo = 5, Title = "Accountant2" });
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Michael2", LastName = "Suyama2", ID = 22, Salary = 700000, ReportsTo = 5, Title = "Accountant2" });
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Robert2", LastName = "King2", ID = 23, Salary = 650000, ReportsTo = 5, Title = "Accountant2" });
+
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Nancy3", LastName = "Davolio3", ID = 24, Salary = 850000, ReportsTo = 6, Title = "Accounts Manager3" });
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Margaret3", LastName = "Peacock3", ID = 25, Salary = 700000, ReportsTo = 6, Title = "Accountant3" });
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Michael3", LastName = "Suyama3", ID = 26, Salary = 700000, ReportsTo = 6, Title = "Accountant3" });
+            employeeDetails.Add(new EmployeeInfo() { FirstName = "Robert3", LastName = "King3", ID = 27, Salary = 650000, ReportsTo = 6, Title = "Accountant3" });
+            return employeeDetails;
+        }
+
+        /// <summary>
+        /// Get the Employees details 
+        /// </summary>
+        /// <returns></returns>
+        private ObservableCollection<EmployeeInfo> GetEmployees()
+        {
+            ObservableCollection<EmployeeInfo> employeeDet = new ObservableCollection<EmployeeInfo>();
+            employeeDet.Add(new EmployeeInfo() { FirstName = "Susmi", LastName = "Joseph", Title = "Management", Salary = 2000000, ReportsTo = -1, ID = 8000 });
+            employeeDet.Add(new EmployeeInfo() { FirstName = "Ramya", LastName = "Fuller", ID = 100, Salary = 1200000, ReportsTo = 2, Title = "Vice President" });
+            employeeDet.Add(new EmployeeInfo() { FirstName = "Priya", LastName = "Leverling", ID = 200, Salary = 1000000, ReportsTo = 2, Title = "GM" });
+            employeeDet.Add(new EmployeeInfo() { FirstName = "Vicky", LastName = "Buchanan", ID = 300, Salary = 900000, ReportsTo = 8, Title = "Manager" });
+            employeeDet.Add(new EmployeeInfo() { FirstName = "Nancy", LastName = "Davolio", ID = 400, Salary = 850000, ReportsTo = 4, Title = "Accounts Manager" });
+            employeeDet.Add(new EmployeeInfo() { FirstName = "Margaret", LastName = "Peacock", ID = 500, Salary = 700000, ReportsTo = 3, Title = "Accountant" });
+            employeeDet.Add(new EmployeeInfo() { FirstName = "Michael", LastName = "Suyama", ID = 600, Salary = 700000, ReportsTo = 3, Title = "Accountant" });
+            employeeDet.Add(new EmployeeInfo() { FirstName = "Robert", LastName = "King", ID = 700, Salary = 650000, ReportsTo = 7, Title = "Accountant" });
+
+            employeeDet.Add(new EmployeeInfo() { FirstName = "Susmi1", LastName = "Joseph1", Title = "Management1", Salary = 2000000, ReportsTo = -1, ID = 9000 });
+            employeeDet.Add(new EmployeeInfo() { FirstName = "Ramya1", LastName = "Fuller1", ID = 800, Salary = 1300000, ReportsTo = 2, Title = "Vice President1" });
+            employeeDet.Add(new EmployeeInfo() { FirstName = "Priya1", LastName = "Leverling1", ID = 900, Salary = 10000000, ReportsTo = 2, Title = "GM1" });
+            employeeDet.Add(new EmployeeInfo() { FirstName = "Vicky1", LastName = "Buchanan1", ID = 1000, Salary = 9000000, ReportsTo = 8, Title = "Manager1" });
+            employeeDet.Add(new EmployeeInfo() { FirstName = "Nancy1", LastName = "Davolio1", ID = 1100, Salary = 860000, ReportsTo = 4, Title = "Accounts Manager1" });
+            employeeDet.Add(new EmployeeInfo() { FirstName = "Margaret1", LastName = "Peacock1", ID = 1200, Salary = 710000, ReportsTo = 3, Title = "Accountant1" });
+            employeeDet.Add(new EmployeeInfo() { FirstName = "Michael1", LastName = "Suyama1", ID = 1300, Salary = 780000, ReportsTo = 3, Title = "Accountant1" });
+            employeeDet.Add(new EmployeeInfo() { FirstName = "Robert1", LastName = "King1", ID = 1400, Salary = 6500000, ReportsTo = 7, Title = "Accountant1" });
+            return employeeDet;
+        }
+        #endregion
+    }
+}
